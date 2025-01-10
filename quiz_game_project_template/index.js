0a7001d73057cfb5570483d788eccb5737e96e25
simmoe <simmoe@gmail.com>
2025-01-10 10:02:59 +0100

    quiz work

--- quiz_game_project_template/index.js
+++ quiz_game_project_template/index.js
@@ -2,17 +2,8 @@ const apiUrl = 'https://opentdb.com/api.php?amount=10&category=12&difficulty=med
 
 function setup() {
   noCanvas() // p5.js setup without canvas
-  fetchQuizQuestion()
 }
 
 async function fetchQuizQuestion() {
-  let res = await fetch(apiUrl)
-  let json = await (res.json())
-  console.log(json.results[0])
-}
 
-function displayQuestion() {
-}
-
-function checkAnswer(selected, button) {
 }
